6fd5d07a205f2191e556f3868b6a6c080c41844a
RafaSaydelles <rafael.ppuerari@gmail.com>
2023-05-30 20:06:16 -0300

    finalizando o projeto

---
+++ index.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('faleConosco');
+
+function change() {
+    btn.style.backgroundColor = '#FFFFFF';
+    btn.style.color = '#C07212';
+}
+
+function back() {
+    btn.style.backgroundColor = '#C07212';
+    btn.style.color = '#FFFFFF';
+}
