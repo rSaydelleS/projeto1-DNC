64796623e2f231a451efc6288151f423ee7139d4
RafaSaydelles <rafael.ppuerari@gmail.com>
2023-05-31 19:04:58 -0300

    aplicando transition nos inputs e finalizando

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const btn = document.getElementById('faleConosco');
 
-function change() {
+function btnChange() {
     btn.style.backgroundColor = '#f98901';
     btn.style.color = '#FFFFFF';
     btn.style.transform = 'translateY(-8px)';
     btn.style.transitionDuration = '1s';
 }
 
-function back() {
+function btnBack() {
     btn.style.backgroundColor = '#C07212';
     btn.style.color = '#FFFFFF';
     btn.style.transform = 'none';
