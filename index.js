f50cb2a67d10214b6fce50e0da8e97ea1b69be1f
RafaSaydelles <rafael.ppuerari@gmail.com>
2023-05-31 18:24:29 -0300

    adicionando o translate e mudando as cores do botão

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const btn = document.getElementById('faleConosco');
 
 function change() {
-    btn.style.backgroundColor = '#FFFFFF';
-    btn.style.color = '#C07212';
+    btn.style.backgroundColor = '#f98901';
+    btn.style.color = '#FFFFFF';
+    btn.style.transform = 'translateY(-8px)';
+    btn.style.transitionDuration = '1s';
 }
 
 function back() {
     btn.style.backgroundColor = '#C07212';
     btn.style.color = '#FFFFFF';
+    btn.style.transform = 'none';
 }
